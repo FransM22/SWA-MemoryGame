ee929d29255994d6b35ee9e419b023aa2c3e23af
FransM22 <fam.manders@student.maastrichtuniversity.nl>
2016-12-08 10:36:07 +0100

    Updated db init script

--- setup_db.sql
+++ setup_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2016 at 03:08 PM
+-- Generation Time: Dec 08, 2016 at 10:35 AM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -41,6 +41,27 @@ INSERT INTO `store_products` (`id`, `title`, `description`, `price`) VALUES
 (1, 'product from the database', 'some description of the product', 103113),
 (2, 'another product', 'description', 10);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password_hash` text NOT NULL,
+  `highscore` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password_hash`, `highscore`) VALUES
+(18, 'admin', '$2y$10$usAp72mo4AqU7z24/4/mq.7sIowPsUFfjdVZhJ2ovHq4Hhjjz2Fme', 1000),
+(20, 'user', '$2y$10$4oOK8c/n6mMBupvs98IvhOraNh5TZ6cPGlJY8TYwZv.EwnQ3aGFAK', 0);
+
 --
 -- Indexes for dumped tables
 --
@@ -52,6 +73,13 @@ ALTER TABLE `store_products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,6 +88,11 @@ ALTER TABLE `store_products`
 --
 ALTER TABLE `store_products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
